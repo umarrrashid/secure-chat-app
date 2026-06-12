612f5937a046041918c720d77996983f6dcdb2df
Umar Rashid <meumarrashid@gmail.com>
2026-06-12 22:02:08 +0530

    Fix msg error

--- app.js
+++ app.js
@@ -1315,8 +1315,6 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-
+server.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
-
 });
